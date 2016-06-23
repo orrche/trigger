eefb079b3f3e4e8c5c36fc069e830de35f27ce78
Kent Gustavsson <kent@minoris.se>
2016-06-23 14:40:37 +0200

    Adds output of request to the response of triggered trigger
    Signed-off-by: Kent Gustavsson <kent@minoris.se>

--- main.go
+++ main.go
@@ -47,6 +47,7 @@ func (state *State) trigger(w http.ResponseWriter, r *http.Request) {
 		for _, trigger := range state.Triggers {
 			if trigger.Id == matches {
 				fmt.Fprintf(w, "Triggered<br/>", trigger.Message)
+				fmt.Fprintf(w, r)
 				trigger.amq.Publish(trigger.Message)
 			}
 		}
